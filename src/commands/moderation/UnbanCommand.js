fa1489480903dfb9fb7cd26ce21890649e71667f
WoozyStudio <laurapinales412@gmail.com>
2023-06-04 16:20:57 -0600

    Create UnbanCommand.js

---
+++ src/commands/moderation/UnbanCommand.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("unban")
+    .setDescription("Unban a user from the server.")
+    .addUserOption((option) => option.setName('id').setDescription('Write the id of the user that you want to unban.').setRequired(true))
+    .addStringOption((option) => option.setName('reason').setDescription('Attach a reason to the sanction, this step is optional.').setRequired(false)),
+    async execute(interaction, client){
+        const user = interaction.options.getUser("id");
+        const reason = interaction.options.getString("reason");
+        interaction.guild.members.unban(user);  
+
+        const BanMessage = new EmbedBuilder()
+        .setAuthor({ iconURL: `${interaction.user.displayAvatarURL({ size: 2048, dynamic: true })}`, name: `${interaction.user.tag} (${interaction.user.id})` })
+        .setColor(0x5CFF9D)
+        .setDescription(`**Member:** ${user} (${user.id})\n**Action:** Unban\n**Reason:** ${reason || "No reason was given for this sanction."}`)
+        .setTimestamp();
+
+        interaction.reply({ embeds: [BanMessage] });
+    }
+}
