d813781a4e68681efc139c614fd86ae5fa873f36
WoozyStudio <laurapinales412@gmail.com>
2023-06-04 16:20:22 -0600

    Create PingCommand.js

--- src/commands/info/PingCommand.js
+++ src/commands/info/PingCommand.js
@@ -5,11 +5,6 @@ module.exports = {
     .setName('ping')
     .setDescription('I will respond with the ping bot in ms.'),
     async execute(interaction, client) {
-        const message = await interaction.deferReply({
-            fetchReply: true
-        });
-
-        const newMessage = `Pong! The API ping is **${client.ws.ping}ms**`
-        await interaction.editReply({ content: newMessage });
+        await interaction.reply({ content: `Pong! The bot ping is **${Date.now() - interaction.createdTimestamp}ms** and the API ping is **${client.ws.ping}ms**.` });
     }
 }
