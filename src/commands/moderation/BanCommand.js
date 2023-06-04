3e052c45e67a900793915d4301c02e279e2214b4
WoozyStudio <laurapinales412@gmail.com>
2023-06-04 16:20:41 -0600

    Create BanCommand.js

---
+++ src/commands/moderation/BanCommand.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Ban a user from the server, you can add a reason if you want.")
+    .addUserOption((option) => option.setName('target').setDescription('Select the user you want to ban.').setRequired(true))
+    .addStringOption((option) => option.setName('reason').setDescription('Attach a reason to the sanction, this step is optional.').setRequired(false))
+    .addStringOption((option) => option.setName('appealable').setDescription('Select whether the sanction is appealable or not.').setRequired(false)
+        .addChoices(
+            {
+                name: "Yes", value: "This sanction can be appealed."
+            },
+            {
+                name: "No", value: "This sanction cannot be appealed."
+            } 
+        )),
+    async execute(interaction, client){
+        const user = interaction.options.getUser("target");
+        const reason = interaction.options.getString("reason");
+        const appealable = interaction.options.getString("appealable");
+        interaction.guild.members.ban(user);  
+
+        const BanMessage = new EmbedBuilder()
+        .setAuthor({ iconURL: `${interaction.user.displayAvatarURL({ size: 2048, dynamic: true })}`, name: `${interaction.user.tag} (${interaction.user.id})` })
+        .setColor(0xFF5C5C)
+        .setDescription(`**Member:** ${user} (${user.id})\n**Action:** Ban\n**Reason:** ${reason || "No reason was given for this sanction."}\n \n> ${appealable || "This sanction can be appealed."}`)
+        .setTimestamp();
+
+        interaction.reply({ embeds: [BanMessage] });
+    }
+}
